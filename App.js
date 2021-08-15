7cb8df0eb40b773eae0ebdfd276594004d01184f
maherkhardani <78689452+maherkhardani@users.noreply.github.com>
2021-08-15 17:52:07 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{border:"solid1pxBlack,max-width:100vw"}}>
+
+ <h1 style={{coler:"red,fontSize:60px"}}>Maher</h1>
+
+
+ <img src={"/imageInSrc.jpg"} />
+
+ <img src={"/imageInPublic.jpg"}/>
+
+
+<vidéo Style={width="320",height="240",contrôles}/>
+
+ <source src={"maVidéo.mp4",type="video/mp4" }/>
+
+</div>
+);
+  }
+
+
+export default App;
